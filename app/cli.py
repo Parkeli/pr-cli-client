9d13d5adb5b1c94dcebe51c4790b9d82db073345
worepix <hry-kurasa@seznam.cz>
2018-11-23 20:27:37 +0100

    Add serial communicationand firebase integration

--- app/cli.py
+++ app/cli.py
@@ -4,13 +4,30 @@
 import click
 import json
 import serial
+from firebase import firebase
+
+firebase_url = "https://parkeli-790b8.firebaseio.com/"
+firebase = firebase.FirebaseApplication(firebase_url, None)
+
 
 
 @click.command()
 @click.option("--run", is_flag=True, help="Run app with config file and connected device")
 @click.argument("configfile", type=click.File("r"), required=True)
+@click.argument("device", required=True)
 
-def cli(run, configfile):
+def cli(run, configfile, device):
     if run:
-        serialPort = serial.Serial(port = "COM4", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-        print(serialPort.readline())
+        uiid = json.loads(configfile.read())["uiid"]
+        serialPort = serial.Serial(port = str(device), baudrate=115200, bytesize=8, stopbits=serial.STOPBITS_ONE)
+        click.echo("Connected")
+        
+        while True:
+                message = str(serialPort.readline()).split(">")[1][1:-2]
+                click.echo(message)
+
+                if message == "Entry":
+                        firebase.patch("/parkings/" + uiid, {"free": firebase.get("/parkings/" + uiid + "/free", None)-1})
+
+                elif message == "Exit":
+                        firebase.patch("/parkings/" + uiid, {"free": firebase.get("/parkings/" + uiid + "/free", None)+1})
