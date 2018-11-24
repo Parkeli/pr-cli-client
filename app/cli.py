2aaf1733b81c1263e5c1fddcad268d125a52ef28
worepix <hry-kurasa@seznam.cz>
2018-11-24 04:24:56 +0100

    Fix

--- app/cli.py
+++ app/cli.py
@@ -10,11 +10,11 @@ firebase_url = "https://parkeli-790b8.firebaseio.com/"
 firebase = firebase.FirebaseApplication(firebase_url, None)
 
 
-
 @click.command()
 @click.option("--run", is_flag=True, help="Run app with config file and connected device")
 @click.argument("configfile", type=click.File("r"), required=True)
 @click.argument("device", required=True)
+
 
 def cli(run, configfile, device):
     if run:
@@ -23,11 +23,36 @@ def cli(run, configfile, device):
         click.echo("Connected")
         
         while True:
-                message = str(serialPort.readline()).split(">")[1][1:-2]
+                message = str(serialPort.readline().replace("\n", ""))
                 click.echo(message)
 
-                if message == "Entry":
-                        firebase.patch("/parkings/" + uiid, {"free": firebase.get("/parkings/" + uiid + "/free", None)-1})
+                if message == '["00bee9d50005/zavory/", "Entry"]':
+                        firebase.patch("/parkings/" + uiid + "/free/actual/", {"free": firebase.get("/parkings/" + uiid + "/free/actual/free", None)-1})
 
-                elif message == "Exit":
-                        firebase.patch("/parkings/" + uiid, {"free": firebase.get("/parkings/" + uiid + "/free", None)+1})
+                elif message == '["00bee9d50005/zavory/", "Exit"]':
+                        firebase.patch("/parkings/" + uiid + "/free/actual", {"free": firebase.get("/parkings/" + uiid + "/free/actual/free", None)+1})
+
+                elif message == '["04962d957d42/A0", "full"]':
+                        freeplace(uiid, "A0", "full")
+
+                elif message == '["04962d957d42/A0", "free"]':
+                        freeplace(uiid, "A0", "free")
+
+
+                elif message == '["7bd09a0a9156/A1", "full"]':
+                        freeplace(uiid, "A1", "full")
+
+
+                elif message == '["7bd09a0a9156/A1", "free"]':
+                        freeplace(uiid, "A1", "free")
+
+
+                elif message == '["698f65464f40/A2", "full"]':
+                        freeplace(uiid, "A2", "full")
+
+                elif message == '["698f65464f40/A2", "full"]':
+                        freeplace(uiid, "A2", "free")
+
+
+def freeplace(uiid, place, state):
+        firebase.patch("/parkings/" + uiid + "/free/places", {place: state})
